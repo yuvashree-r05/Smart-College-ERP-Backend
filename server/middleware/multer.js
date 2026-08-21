e7d9fbb75d5afb4d32d5a57fc8d1a976b5611c4d
apnacollege-student <yuvashree@users.noreply.github.com>
2026-08-21 12:19:38 +0530

    updated multer.js

--- server/middleware/multer.js
+++ server/middleware/multer.js
@@ -2,7 +2,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
     destination(req, file, cb){
-        cb(null,"uploads/");
+        cb(null,path.join(__dirname, "../uploads"));
     },
     filename: function(req, file,cb){
        cb(null,Date.now()+"-"+file.originalname);
